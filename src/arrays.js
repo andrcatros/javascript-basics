f10622e7678f691a9a246955cd678a63b6b0abeb
anna-m-b <anna.balquin@gmail.com>
2020-10-22 21:21:19 +0100

    made a start on the array exercises

--- src/arrays.js
+++ src/arrays.js
@@ -1,41 +1,45 @@
 const getNthElement = (index, array) => {
-  // your code here
-};
+  let i =  index > array.length ? index - array.length : index
+  console.log(index, array,i)
+  return array[i];
+  };
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.join(',');
 };
 
 const csvStringToArray = string => {
-  // your code here
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
+ array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+ return [...array, element];
 };
 
+
 const removeNthElement = (index, array) => {
-  // your code here
+  array.splice(index, 1);
+  return array
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+  return numbers.map(num => num.toString());
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  return strings.map(string => string.toUpperCase());
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
+//
 };
 
 const onlyEven = numbers => {
-  // your code here
+  return numbers.filter(num => num % 2 === 0)
 };
 
 const removeNthElement2 = (index, array) => {
