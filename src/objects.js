e7f5c9cf433cba13d407143121a08f04ee39054f
anna-m-b <anna.balquin@gmail.com>
2020-10-24 15:20:50 +0100

    finished

--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,54 @@
 const createPerson = (name, age) => {
-  // your code here
-};
+  return {
+          name: name,
+          age: age
+        }
+}
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
+
 const hasProperty = (property, object) => {
-  // your code here
+  return Boolean(object[property]);
 };
 
 const isOver65 = person => {
-  // your code here
+  return person.age > 65;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person => person.age)
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.filter(people => people.name === name)[0];
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(car => car.manufacturer === "Honda");
 };
 
 const averageAge = people => {
-  // your code here
+  const ages = people.map(person => person.age);
+  return ages.reduce((total, age) => total + age) / ages.length;
+  
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return {
+          name: name,
+          age: age,
+          introduce: function(name2){
+            return `Hi ${name2}, my name is ${name} and I am ${age}!`
+          }
+  }
+ 
 };
 
 module.exports = {
