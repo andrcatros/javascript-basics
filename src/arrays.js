054b59444f3c3e1c8ccbf9ee3282fa91e5cdd81d
anna-m-b <anna.balquin@gmail.com>
2020-10-24 11:55:08 +0100

    finished booleans and arrays

--- src/arrays.js
+++ src/arrays.js
@@ -1,8 +1,7 @@
 const getNthElement = (index, array) => {
-  let i =  index > array.length ? index - array.length : index
-  console.log(index, array,i)
+  let i =  index >= array.length ? index - array.length : index;
   return array[i];
-  };
+};
 
 const arrayToCSVString = array => {
   return array.join(',');
@@ -23,7 +22,7 @@ const addToArray2 = (element, array) => {
 
 const removeNthElement = (index, array) => {
   array.splice(index, 1);
-  return array
+  return array;
 };
 
 const numbersToStrings = numbers => {
@@ -35,32 +34,52 @@ const uppercaseWordsInArray = strings => {
 };
 
 const reverseWordsInArray = strings => {
-//
+  return strings.map(string => string.split('').reverse().join(''));
 };
 
 const onlyEven = numbers => {
-  return numbers.filter(num => num % 2 === 0)
+  return numbers.filter(num => num % 2 === 0);
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  const array2 = [...array];
+  array2.splice(index, 1);
+  return array2;
 };
 
+
 const elementsStartingWithAVowel = strings => {
-  // your code here
+  const regex = /a|e|i|o|u/i;
+  return strings.filter(string => regex.test(string[0]))
 };
 
 const removeSpaces = string => {
-  // your code here
+  return string.split('').filter(elem => elem !== " ").join('');
+};
+
+const removeSpaces2 = string => {
+  return string.replace(/\s/g, "");
 };
 
 const sumNumbers = numbers => {
-  // your code here
+  return numbers.reduce((total, num) => total + num)
 };
 
+// numbers = [1, 3, 5, 6, 2, 8]
+// on first loop, total is 1 and num is 3
+// 1 + 3 = 4 so total becomes 4 and num moves to the next val in the array, which is 5
+// 4 + 5 = 9, so total is now 9 and we loop around to the next val, 6
+// total + num === 9 + 6 = 15
+// on the next loop we add 15 + 2
+// now total is 17 and num becomes 8, the last value.
+// 17 + 8 = 25. 8 is the last element in the array so our accumulator, total in this case, is returned.
+
 const sortByLastLetter = strings => {
-  // your code here
+  const backwards = reverseWordsInArray(strings).sort();
+  return reverseWordsInArray(backwards)
 };
+
+
 
 module.exports = {
   getNthElement,
--- src/booleans.js
+++ src/booleans.js
@@ -3,59 +3,61 @@ const negate = a => {
 };
 
 const both = (a, b) => {
-  return a && b;
-};
+  return Boolean(a && b);
+}
+
 
 const either = (a, b) => {
-  return a || b;
+  return Boolean(a || b);
 };
 
 const none = (a, b) => {
-  return !a && !b;
+  return Boolean(!a && !b);
 };
 
 const one = (a, b) => {
-  // your code here
+  return Boolean(a && !b || !a && b);
 };
 
+
 const truthiness = a => {
-  // your code here
+  return Boolean(a);
 };
 
 const isEqual = (a, b) => {
-  // your code here
+  return a === b;
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+  return a > b;
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+  return a <= b;
 };
 
 const isOdd = a => {
-  // your code here
+  return (a % 2) > 0;
 };
 
 const isEven = a => {
-  // your code here
+  return (a % 2) === 0;
 };
 
 const isSquare = a => {
-  // your code here
+  return Number.isInteger(Math.sqrt(a));
 };
 
 const startsWith = (char, string) => {
-  // your code here
+  return char === string[0];
 };
 
 const containsVowels = string => {
-  // your code here
+  return /a|e|i|o|u/gi.test(string)
 };
 
 const isLowerCase = string => {
-  // your code here
+  return string === string.toLowerCase();
 };
 
 module.exports = {
